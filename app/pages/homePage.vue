99af5bfe64e099993219a7d5d1ff88bca383538d
userNameGoes-Here <mrompitso@gmail.com>
2026-05-09 23:34:22 +0200

    Basic search bar add

--- app/pages/homePage.vue
+++ app/pages/homePage.vue
@@ -3,5 +3,13 @@
 </script>
 
 <template>
+    <main>
+        <h1>Search</h1>
 
+        <form @submit.prevent="searchPosts">
+            <input v-model="searchTerm" type="text" placeholder="Search"/>
+
+            <button type="submit">Search</button>
+        </form>
+    </main>
 </template>
