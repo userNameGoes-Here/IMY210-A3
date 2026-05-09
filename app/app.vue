a9750bd252b18d44545b9581f52b936aab4919b0
userNameGoes-Here <mrompitso@gmail.com>
2026-05-09 22:11:33 +0200

    Interface def (placing everything in the center)

--- app/app.vue
+++ app/app.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
+  <div id="body">
+
+  </div>
   
 </template>
 
 <style scoped>
-
+  #id {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
 </style>
