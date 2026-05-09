2062c3c9e63d89602b92d5a3ccfe5079d1ed7ed6
userNameGoes-Here <mrompitso@gmail.com>
2026-05-09 23:41:22 +0200

    script update (variable and function definitions)

--- app/pages/homePage.vue
+++ app/pages/homePage.vue
@@ -1,4 +1,37 @@
 <script setup>
+    const API_URL = 'http://localhost:1337';
+
+    const searchTerm = ref('');
+    const posts = ref([]);
+    const loading = ref(false);
+    const error = ref('');
+    const searched = ref(false);
+
+    async function searchPosts() {
+        if (searchTerm.value === '') {
+            posts.value = []
+            searched.value = false
+            return
+        }
+
+        loading.value = true
+        error.value = ''
+        searched.value = true
+
+        try {
+            const url = API_URL + '/api/posts?populate=*' + '&filters[$or][0][title][$containsi]=' + searchTerm.value + '&filters[$or][1][author][name][$containsi]=' + searchTerm.value;
+            const response = await fetch(url);
+            const result = await response.json();
+
+            posts.value = result.data;
+        } 
+    
+        catch (err) {
+            error.value = 'Search failed';
+        }
+
+        loading.value = false;
+}
 
 </script>
 
